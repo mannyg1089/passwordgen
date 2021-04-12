2fc573f18fd2f32a93897ac0813412c7461840a0
Manny <jmgonzalez1089@gmail.com>
2021-04-11 23:51:10 -0700

    submit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var numbr = "0,1,2,3,4,5,6,7,8,9";
 var spcl = "`,!,@,#,$,%,^,&,*";
 
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -24,14 +25,17 @@ generateBtn.addEventListener("click", writePassword);
 
 // Make code work
 
-function generatePassword()
+function generatePassword(){
 
 
-
-
-{
-   prompt ("What length do you want on your password, Must be between 8-128");
-
-   
+ generatePassword =  prompt ("What length do you want on your password Must be between 8-128")
 }
+
+if (generatePassword  >=8 || generatePassword <= 128) {
+;
+} else if (generatePassword >= 8 || generatePassword <= 128){
+  generatePassword = prompt ("Choose between 8 and 128")
+}
+
+
 // options for password
