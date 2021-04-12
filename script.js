26b6384f3da97c47fa4baeff21dc93a7977515f2
Manny <jmgonzalez1089@gmail.com>
2021-04-11 21:33:09 -0700

    go live

---
+++ script.js
@@ -0,0 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var lowerCase = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
+
+var uprCase = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+
+var numbr = "0,1,2,3,4,5,6,7,8,9";
+
+var spcl = "`,!,@,#,$,%,^,&,*";
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Make code work
+
+function generatePassword()
+
+
+
+
+{
+   prompt ("What length do you want on your password, Must be between 8-128");
+
+   
+}
+// options for password
